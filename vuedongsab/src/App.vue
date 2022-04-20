a4f49984c621266b591c43aeaff639a3f60531e8
jiyeonyun <jiyeonyun43@gmail.com>
2022-04-20 16:09:56 +0900

    이벤트 핸들러

--- vuedongsab/src/App.vue
+++ vuedongsab/src/App.vue
@@ -1,11 +1,24 @@
 <template>
   <div class="nav">
-    <a v-for="menu in menus" :key="menu">{{menu}}</a>
+    <a v-for="(menu,i) in menus" :key="i">{{menu}}</a>
   </div>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <div v-for="product in products" :key="product">
-    <h4>{{product}}</h4>
-    <p>80만원</p>
+  <div>
+    <h4>{{products[0]}}</h4>
+    <p>50만원</p>
+    <button @click="increase(0)">허위매물신고</button>
+    <span>신고수 : {{신고수[0]}} </span>
+  </div>
+  <div>
+    <h4>{{products[1]}}</h4>
+    <p>50만원</p>
+    <button @click="increase(1)">허위매물신고</button>
+    <span>신고수 :{{신고수[1]}} </span>
+  </div>
+  <div>
+    <h4>{{products[2]}}</h4>
+    <p>50만원</p>
+    <button @click="increase(2)">허위매물신고</button>
+    <span>신고수 : {{신고수[2]}}</span>
   </div>
 </template>
 
@@ -16,9 +29,14 @@ export default {
   name: 'App',
   data(){
     return{
+      신고수:[0,0,0],
       products : ['역삼동원룸', '천호동원룸', '마포구원룸'],
       menus : ['home' , 'about' , 'products'],
     }
+  },
+  //함수만드는 공간
+  methods:{
+    increase(a){this.신고수[a]++} //this.써야 데이터 사용가능
   },
   components: {
   }
@@ -32,7 +50,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 .nav{
   background: darkslateblue;
