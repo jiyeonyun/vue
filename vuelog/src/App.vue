70af3b9b7cf950ac92af733afa9d2e18d96aef09
jiyeonyun <jiyeonyun43@gmail.com>
2022-04-21 12:17:24 +0900

    vuelog

---
+++ vuelog/src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Features</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Pricing</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled">Disabled</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+</nav>
+  <div class="container mt-4">
+    <h5> vue로 만드는 블로그</h5>
+    <List v-for="(post,a) in posts" :key="a" :posts='posts[a]'/>
+  </div>
+</template>
+
+
+<script>
+import List from './components/list.vue';
+import post from './assets/post.js';
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      posts : post
+    }
+  },
+  components: {
+    List,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ vuelog/src/components/list.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h5>{{posts.title}}</h5>
+        <p>{{posts.date}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'List-blog',
+    props:{
+        posts : Object,
+    }
+}
+</script>
+
+<style>
+
+</style>
